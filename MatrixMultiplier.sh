06b62590b90f83af912d6321c5c71498ccb6f9ca
abdulbadii <abdulbadii@yandex.com>
2021-09-05 16:47:42 +0000

    new func: rot i.e. rotate 2D on cartesian

--- MatrixMultiplier.sh
+++ MatrixMultiplier.sh
@@ -14,6 +14,40 @@ mul(){ ##### BEGINNING OF mul, matrix multiplier #####
 				M=`bc<<<"$M+${arr1[r*CR+cr]}*${arr2[cr*C+c]}"`;}
 			printf -v arr[r*C+c] %.${P}f $M
 };}
-	for((r=0;r<$R;r++)){ echo
-		for((c=0;c<$C;c++)){	echo -n ${arr[r*$C+c]}\ ;};}
-} ##### ENDING OF mul, matrix #####
+	for((r=0;r<$R;r++)){
+		for((c=0;c<$C;c++)){	echo -n ${arr[r*$C+c]}\ ;}
+		echo;}
+}
+rot(){
+	local -n arr1=$1; p=3.1415926535898
+	P=$3;D=$2
+	[ -z "$P" ] &&{
+		if [[ "$2" = P=[0-9]* ]] ;then P=${2#P=};D=45
+		else
+			P=0
+			if [ "$2" = - ] ;then D=-45
+			elif [ "$2" ] ;then D=$2
+			else D=45;fi
+		fi
+	}
+	d=`bc -l<<<"$D/180*$p"`
+	cos=`bc -l<<<"c($d)"`
+	sin=`bc -l<<<"s($d)"`
+	minsin=`bc -l<<<"-1*$sin"`
+	R=${arr1[@]}
+	CR=(${R%%,*}); CR=${#CR[@]}
+	R=${R//[^,]}; let R=${#R}+1
+	arr1=(${arr1[@]//,/ })
+	arr2=($cos $minsin $sin $cos)
+	C=2
+	for((r=0;r<$R;r++)){
+		for((c=0;c<$C;c++)){	M=0
+			for((cr=0;cr<$CR;cr++)){
+				M=`bc<<<"$M+${arr1[r*CR+cr]}*${arr2[cr*C+c]}"`;}
+			printf -v arr[r*C+c] %.${P}f $M
+};}
+echo "Rotated by $D degree based on cartesian system (counter clockwise)"
+	for((r=0;r<$R;r++)){
+		for((c=0;c<$C;c++)){	echo -n ${arr[r*$C+c]}\ ;}
+		echo;}
+} ##### ENDING OF rot, matrix #####
